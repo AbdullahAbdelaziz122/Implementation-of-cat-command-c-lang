867ac6be117cf6864e4b565ae08829660b627798
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-07-19 02:47:05 +0300

    initial implementaion and handling of argumens and parsing arguments --help

--- src/main.c
+++ src/main.c
@@ -1,6 +1,42 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    printf("Hello, World\n");
+typedef struct arguments{
+
+unsigned int files_count;
+char **files;
+
+}arguments;
+
+void parse_arguments(int argc, char **argv, arguments *args){
+    args->files = malloc(argc * sizeof(char *));
+    int index = 0;
+
+
+    for(int i = 1; i < argc; i++){
+        if(strcmp(argv[i], "--help") == 0){
+            printf("./main <files> [--help]\n");
+            exit(0);
+        } else {
+            args-> files[index] = argv[i];
+            index += 1;
+        }
+    }
+
+    args->files_count = index;
+}
+
+
+
+int main(int argc, char **argv){
+
+    arguments args = {0};
+    parse_arguments(argc, argv, &args);
+
+    printf("%d\n", args.files_count);
+
+    printf("Hello, World!\n");
     return 0;
 }
+
